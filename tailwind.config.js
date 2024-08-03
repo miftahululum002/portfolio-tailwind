30a5be0bfb41c6621af7af34a8cdf5e84ce0e3cf
O027-MIFTAHUL ULUM S.Pd <ulumiftahul06@gmail.com>
2024-08-04 06:58:30 +0700

    all

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  // content: ["./src/**/*.{html,js}"],
+  content: ["./src/index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#14b8a6",
+        dark: "#1e293b",
+      },
+    },
   },
   plugins: [],
 };
