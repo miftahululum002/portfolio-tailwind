9856e87887a0a8054275e258521363fab45f8852
O027-MIFTAHUL ULUM S.Pd <ulumiftahul06@gmail.com>
2024-08-04 17:24:51 +0700

    config tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,18 @@ module.exports = {
   // content: ["./src/**/*.{html,js}"],
   content: ["./src/index.html"],
   theme: {
+    container: {
+      center: true,
+      padding: "16px",
+    },
     extend: {
       colors: {
-        primary: "#14b8a6",
-        dark: "#1e293b",
+        primary: "#06b6d4",
+        secondary: "#64748b",
+        dark: "#164e63",
+      },
+      screens: {
+        "2xl": "1320px",
       },
     },
   },
