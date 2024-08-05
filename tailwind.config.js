1236761585e141982e5360b9fb0568dbb9998403
O027-MIFTAHUL ULUM S.Pd <ulumiftahul06@gmail.com>
2024-08-05 20:06:17 +0700

    config tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   // content: ["./src/**/*.{html,js}"],
-  content: ["./index.html"],
+  content: ["./index.html", "blog.html"],
   theme: {
     container: {
       center: true,
