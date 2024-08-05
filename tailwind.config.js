dad34678ac6bddc37d04c7bfb850e7bc75638172
O027-MIFTAHUL ULUM S.Pd <ulumiftahul06@gmail.com>
2024-08-05 18:45:05 +0700

    refaktor code

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   // content: ["./src/**/*.{html,js}"],
-  content: ["./src/index.html"],
+  content: ["./index.html"],
   theme: {
     container: {
       center: true,
