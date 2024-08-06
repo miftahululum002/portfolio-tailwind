fadfef6225bd4e9d3db68136108eeec54e344a5b
O027-MIFTAHUL ULUM S.Pd <ulumiftahul06@gmail.com>
2024-08-06 07:02:06 +0700

    remove console

--- dist/js/script.js
+++ dist/js/script.js
@@ -42,7 +42,7 @@ const blogData = [
   },
 ];
 window.addEventListener("load", (event) => {
-  console.log("page is fully loaded");
+  // console.log("page is fully loaded");
   // getTemplateBlog(blogData);
 });
 window.addEventListener("DOMContentLoaded", function () {
