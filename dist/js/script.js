46d957e9d4427df2b1017f9f547385af367cae48
O027-MIFTAHUL ULUM S.Pd <ulumiftahul06@gmail.com>
2024-08-06 09:11:39 +0700

    dark mode belum selesai

--- dist/js/script.js
+++ dist/js/script.js
@@ -3,11 +3,16 @@
 window.onscroll = function () {
   const header = document.querySelector("header");
   const fixedNav = header.offsetTop;
+  const toTop = document.querySelector("#to-top");
 
   if (window.pageYOffset > fixedNav) {
     header.classList.add("navbar-fixed");
+    toTop.classList.remove("hidden");
+    toTop.classList.add("flex");
   } else {
     header.classList.remove("navbar-fixed");
+    toTop.classList.add("hidden");
+    toTop.classList.remove("flex");
   }
 };
 //  Hamburger
@@ -18,6 +23,23 @@ hamburger.addEventListener("click", function () {
   navMenu.classList.toggle("hidden");
 });
 
+// klik diluar humberger
+window.addEventListener("click", function (e) {
+  if (e.target != hamburger && e.target != navMenu) {
+    hamburger.classList.remove("humberger-active");
+    navMenu.classList.add("hidden");
+  }
+});
+//  dark mode toggle
+const darkToggle = document.querySelector("#dark-toggle");
+const html = document.querySelector("html");
+darkToggle.addEventListener("click", function () {
+  if (darkToggle.checked) {
+    html.classList.add("dark");
+  } else {
+    html.classList.remove("dark");
+  }
+});
 const blogData = [
   {
     title: "Tips Belajar Programming",
